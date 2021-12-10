d6914d0d334f657692505d045d5db4f3171d611a
KevinDanae <kevin.danaea@gmail.com>
2021-12-09 23:12:30 -0600

    Instalacion inicial de Vuex

---
+++ src/components/Counter.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1>Counter en Vuex</h1>
+  <h2>Acceso directo: {{ $store.state.count }}</h2>
+  <h2>Computed: {{ countComputed }}</h2>
+  <h1>MapState</h1>
+  <h2>MapState: {{ counter }}</h2>
+  <h2>{{ mutation }}</h2>
+</template>
+
+<script>
+import { mapState } from "vuex";
+
+export default {
+  computed: {
+    countComputed() {
+      return this.$store.state.count;
+    },
+    ...mapState({
+      counter: (state) => state.count,
+      mutation: (state) => state.lastMutation,
+    }),
+  },
+};
+</script>
+
+<style></style>
