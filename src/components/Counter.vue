2f7f15b8f1e41575626822850ab33295926d7d17
KevinDanae <kevin.danaea@gmail.com>
2021-12-10 00:09:28 -0600

    Update: Se generan mutations y actions iniciales

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -2,13 +2,18 @@
   <h1>Counter en Vuex</h1>
   <h2>Acceso directo: {{ $store.state.count }}</h2>
   <h2>Computed: {{ countComputed }}</h2>
+
+  <button @click="increment">+1</button>
+  <button @click="incrementBy">+5</button>
+  <button @click="incrementRandomInt">random</button>
+
   <h1>MapState</h1>
   <h2>MapState: {{ counter }}</h2>
   <h2>{{ mutation }}</h2>
 </template>
 
 <script>
-import { mapState } from "vuex";
+import { mapState, mapActions } from "vuex";
 
 export default {
   computed: {
@@ -19,6 +24,15 @@ export default {
       counter: (state) => state.count,
       mutation: (state) => state.lastMutation,
     }),
+  },
+  methods: {
+    increment() {
+      this.$store.commit("increment");
+    },
+    incrementBy() {
+      this.$store.commit("incrementBy", 5);
+    },
+    ...mapActions(["incrementRandomInt"]),
   },
 };
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,30 @@
 import { createStore } from "vuex";
+import getRandomInt from "../helpers/getRandomInt";
 
 export default createStore({
   state: {
     count: 0,
     lastMutation: "none",
+    isLoading: false,
+    randomInt: 0,
+  },
+
+  mutations: {
+    increment(state) {
+      state.count++;
+      state.lastMutation = "increment";
+    },
+    incrementBy(state, val) {
+      state.count += val;
+      state.lastMutation = "incrementBy";
+      state.randomInt = val;
+    },
+  },
+
+  actions: {
+    async incrementRandomInt(context) {
+      const randomInt = await getRandomInt();
+      context.commit("incrementBy", randomInt);
+    },
   },
 });
